aee93c23c97c58ba79b2982a8bc4661a7d63d076
fuadtamton <fuadt@factweavers.com>
2019-02-13 18:52:42 +0530

    add patch in todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/Todo')
@@ -29,6 +30,7 @@ app.get('/todos', (req, res) => {
 })
 
 app.get('/todos/:id', (req, res) => {
+
     const id = req.params.id;
     if (!ObjectID.isValid(id)) {
         return res.status(404).send()
@@ -58,6 +60,33 @@ app.delete('/todos/:id', (req, res) => {
             res.send({ todo });
         })
         .catch(e => res.status(400).send())
+})
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed'])
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send()
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+        .then(todo => {
+            if (!todo) {
+                res.status(404).send();
+            }
+            res.send({ todo });
+        })
+        .catch(e => {
+            res.status(400).send()
+        })
+
 })
 
 app.listen(3000, () => {
