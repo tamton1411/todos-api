ff3b79aebdabac0b0156bf382145ff395a5cd7b0
fuadtamton <fuadt@factweavers.com>
2019-02-13 11:53:29 +0530

    test get route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const expect = require('expect');
 const { app } = require('../server');
 const { Todo } = require('../models/Todo')
 
+
+const todos = [{
+    text: "Test todo1"
+}, {
+    text: "test todo2"
+}]
 beforeEach(done => {
-    Todo.deleteMany({}).then(() => done());
+    Todo.remove().then(() => {
+        return Todo.insertMany(todos)
+    }).then(() => done());
 })
 
 describe("POST /todos", () => {
@@ -24,7 +32,7 @@ describe("POST /todos", () => {
                     return done(err)
                 }
 
-                Todo.find().then(todos => {
+                Todo.find({ text }).then(todos => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text)
                     done()
@@ -36,17 +44,26 @@ describe("POST /todos", () => {
             .post('/todos')
             .send({})
             .expect(400)
-            .expect(res => {
-                expect(res.body.text).toBe(undefined)
-            })
             .end((err, res) => {
                 if (err) {
                     return done(err)
                 }
                 Todo.find().then(todos => {
-                    expect(todos.length).toBe(0)
+                    expect(todos.length).toBe(2)
                     done()
                 }).catch(e => done(e))
             })
+    })
+})
+
+describe("GET /todos", () => {
+    it("should get all todos", (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todos.length).toBe(2)
+            })
+            .end(done)
     })
 })
