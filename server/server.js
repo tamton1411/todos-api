5cfec787818b06689be53ded1533bc2fcaa15249
fuadtamton <fuadt@factweavers.com>
2019-02-13 13:02:56 +0530

    add findbyid in todos route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/Todo')
@@ -23,8 +24,25 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find()
         .then(todos => {
-            res.send({todos})
+            res.send({ todos })
         }, e => res.status(400).send(e))
+})
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send()
+    }
+    Todo.findById(id)
+        .then(todo => {
+            if (!todo) {
+                return res.status(404).send()
+            }
+            res.send({todo})
+        })
+        .catch(e => {
+            res.status(404).send()
+        })
 })
 
 app.listen(3000, () => {
