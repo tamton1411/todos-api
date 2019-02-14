fceb65bc3a3b4adde622ad97d0873943dd691be2
fuadtamton <fuadt@factweavers.com>
2019-02-14 16:16:49 +0530

    hash user passowrd before saving ti database

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,22 +1,41 @@
 // const { SHA256 } = require("crypto-js");
 
+
 // const msg = "fuad tamton";
 // const hash = SHA256(msg).toString()
-
 // console.log("Message ", msg)
 // console.log("Hash ", hash)
 
-const jwt = require('jsonwebtoken');
 
-const data = {
-    id: 10
-}
 
-console.log(data)
 
-const en = jwt.sign(data, 'fuadtamton')
-console.log(en)
+// const jwt = require('jsonwebtoken');
+// const data = {
+//     id: 10
+// }
+// console.log(data)
 
-const de = jwt.verify(en, 'fuadtamton')
+// const en = jwt.sign(data, 'fuadtamton')
+// console.log(en)
 
-console.log(de)
+// const de = jwt.verify(en, 'fuadtamton')
+
+// console.log(de)
+
+
+
+
+const bcrypt = require('bcryptjs');
+
+const password = "123abc!";
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash)
+//     })
+// })
+
+const hashpass = '$2a$10$OVPuFJ7T6W2eMCiSySOYD.xkkUjgHOAR3Xv/fzWY7PFIKC6E8.INS';
+bcrypt.compare(password, hashpass, (err, res) => {
+    console.log(res)
+})
--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -65,6 +66,22 @@ userSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     });
 };
+
+userSchema.pre('save', function (next) {
+    var user = this;
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            })
+        })
+    }
+    else {
+        next();
+    }
+});
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
