0b8a2cfb386a082a2db248b83d33ad1e0a62e590
fuadtamton <fuadt@factweavers.com>
2019-02-15 11:35:50 +0530

    test DELETE /users/me/token  route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -249,7 +249,7 @@ describe("POST users/login", () => {
                 }
                 User.findById(users[0]._id).then(user => {
                     expect(user.tokens[0]).toInclude({
-                            access: 'auth',
+                        access: 'auth',
                         token: res.header['x-auth']
                     })
                     done()
@@ -271,4 +271,23 @@ describe("POST users/login", () => {
             .end(done)
     })
 
+})
+
+describe("DELETE /users/me/token", () => {
+    it("should remove auth token on logout", (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+
+                User.findById(users[0]._id).then(user => {
+                    expect(user.tokens.length).toBe(0)
+                    done()
+                }).catch(err => done(err))
+            })
+    })
 })
